ddf3d10d8c0db70f2534524a39217dfd7f421ecc
Ing. Martin Procházka (teacher) <prochazka@voskh.cz>
2026-09-24 15:13:48 +0200

    Implement basic calculator for summing two numbers
    Added a simple calculator that sums two numbers with input and output.

--- Kapitola 10/kalkulacka.py
+++ Kapitola 10/kalkulacka.py
@@ -1,4 +1,12 @@
+# JEDNODUCHÁ SČÍTAČKA
+# Ukázka konverzních funkcí zajišťující převody mezi číselnými a textových datovým typem
+
+# vstup
 cislo1 = input("Zadej 1. číslo: ")
 cislo2 = input("Zadej 2. číslo: ")
+
+# výpočet
 vysledek = int(cislo1) + float(cislo2)
+
+# výstup
 print("Výsledek: " + str(vysledek))
